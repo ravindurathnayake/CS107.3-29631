5833ed1884bf19685b245aa0794db53ff447a353
Ravindu Rathnayake <132426316+ravindurathnayake@users.noreply.github.com>
2024-02-15 07:31:38 +0530

    Add files via upload

---
+++ Lab 01/Q4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of terms for the Fibonacci series:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Fibonacci series:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}

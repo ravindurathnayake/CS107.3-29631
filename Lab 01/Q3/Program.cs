ea73f6c35d28af9bd373698347b40bbae35cf55e
Ravindu Rathnayake <132426316+ravindurathnayake@users.noreply.github.com>
2024-02-15 07:31:17 +0530

    Add files via upload

---
+++ Lab 01/Q3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Positive Number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for(int i=1;i<=num;i++)
+            {
+                
+                if(num<0)
+                {
+                    Console.WriteLine("ERROR");
+                    break;
+                }
+                else
+                {
+                    sum += i;
+                }
+
+            }
+            if(sum>=0)
+            {
+                Console.WriteLine($"The Sum of the Numbers is : {sum}");
+                
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

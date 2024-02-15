6fddc8512417e5dca403b37a29951f41ab99e4b7
Ravindu Rathnayake <132426316+ravindurathnayake@users.noreply.github.com>
2024-02-15 07:31:58 +0530

    Add files via upload

---
+++ Lab 01/Q5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number : ");
+            int num= Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i <=10; i++)
+            { 
+                Console.WriteLine($"{num}*{i} = {num*i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
